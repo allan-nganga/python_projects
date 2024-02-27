cde12e8984cbafbaf521dd776a4abd72df2d63c7
allan-nganga <allen.nganga7@gmail.com>
2024-02-27 21:16:52 +0300

    Initial commit. Week one assignment: Collecting user input

---
+++ python.py
@@ -0,0 +1,5 @@
+name = input("Enter you name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Hi " +name, "You are " +age, "years old and live in " +location)
